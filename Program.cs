75f64cbfac5d1d5ce62c3a733b63e7cd24d7fc4c
Aid.Gladki <Aid.Gladki@S26-5CG53818PW>
2026-02-13 22:54:00 +1100

    first try at 11 pm help

--- Program.cs
+++ Program.cs
@@ -1,5 +1,43 @@
-﻿// Program Name: 
-// Created on: 
+﻿// Program Name: Aiden quizz
+// Created on: 13/02/2026
 // Author: Aiden Gladki
+// star level 2
 
+int score = 0;
+Console.WriteLine("HELLO AND WELCOME TO THE AIDEN GAMES!!!");
+Console.WriteLine("Tell me whats your name");
+string name = Console.ReadLine();
+Console.WriteLine("...");
+Console.WriteLine($"SO {name} ON TO OUR FIRST ROUND");
+Console.WriteLine("WHATS AIDENS LAST NAME!!!");
+Console.WriteLine("...");
 
+string lastnaem = Console.ReadLine();
+if (lastnaem == "gladki")
+{
+    Console.WriteLine($"Congardulation {name} you must know Aiden well");
+    score++;
+}
+else
+{
+    Console.WriteLine($"Ohhhhh nice try well it is your first time so ill let fo off this time {name}");
+}
+Console.WriteLine("...");
+Console.WriteLine($"ON TOOOO THE SECOND ROUND TELL ME {name} whats Aidens favorite color?");
+Console.WriteLine("...");
+string color = Console.ReadLine();
+if (color == "purple")
+{
+    Console.WriteLine($"Ohhhh good job not many people know that {name}");
+    score++;
+}
+else if (color == "red")
+{
+    Console.WriteLine($"Ohhhhh nice try well very close Aiden's favorite color is Purple, not red");
+}
+else
+{
+    Console.WriteLine("Haaa... tisk tisk tisk");
+    Console.WriteLine($"Disapointing Aidens favorite color is Purple not {color}");
+    score--;
+}
